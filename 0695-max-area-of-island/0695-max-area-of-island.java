128f21410e6d708c2e4dc497a4ed7b7b79d58079
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2022-12-26 17:26:51 -0800

    Time: 2 ms (100.00%), Space: 42.3 MB (92.56%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,74 +1,24 @@
 class Solution {
-    Map<String,Boolean> visited = new HashMap<>();
-    int m,n;
-    public int bfs(int i1, int j1, int[][] grid){
-        //System.out.println("source: "+i1+" "+j1);
-        visited.put(i1+" "+j1,true);
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        q1.add(i1);
-        q2.add(j1);
-        int area = 1;
-        while(!q1.isEmpty()){
-            int i = q1.poll();
-            int j = q2.poll();
-            String s1 = (i-1)+" "+j;
-            String s2 = (i+1)+" "+j;
-            String s3 = i+" "+(j-1);
-            String s4 = i+" "+(j+1);
-            if(i-1>=0){
-                if(visited.get(s1)==null && grid[i-1][j]==1){
-                    //System.out.println(adj.get(node).get(i));
-                    q1.add(i-1);
-                    q2.add(j);
-                    visited.put(s1,true);
-                    area++;
-                }
-            }
-            if(i+1<m){
-                if(visited.get(s2)==null && grid[i+1][j]==1){
-                    //System.out.println(adj.get(node).get(i));
-                    q1.add(i+1);
-                    q2.add(j);
-                    visited.put(s2,true);
-                    area++;
-                }
-            }
-            if(j-1>=0){
-                if(visited.get(s3)==null && grid[i][j-1]==1){
-                    //System.out.println(adj.get(node).get(i));
-                    q1.add(i);
-                    q2.add(j-1);
-                    visited.put(s3,true);
-                    area++;
-                }
-            }
-            if(j+1<n){
-                if(visited.get(s4)==null && grid[i][j+1]==1){
-                    //System.out.println(adj.get(node).get(i));
-                    q1.add(i);
-                    q2.add(j+1);
-                    visited.put(s4,true);
-                    area++;
-                }
-            }
-            
-        }
-        return area;
-    }
-    public int maxAreaOfIsland(int[][] grid) {
-        int area=0;
-        m = grid.length;
-        n = grid[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(visited.get(i+" "+j)==null && grid[i][j]==1){ 
-                    area = Math.max(bfs(i,j,grid),area);
-                }
-            }
+    public int dfs(int[][] grid, int row, int col){
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0)
+            return 0;
+        grid[row][col] = 0; // mark as visited
         
+        // there is no need to give 1 back to grid like gird[row][col] = 1
+		// dont forget to add 1 before return
+        return 1 + dfs(grid, row, col-1) + dfs(grid, row+1, col) + dfs(grid, row, col+1) + dfs(grid, row-1, col);
+    }
+    
+    public int maxAreaOfIsland(int[][] grid) {
+        
+        int max = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1)
+                    max = Math.max(max, dfs(grid, i, j));
+            }
         }
-
-        return area;
+        
+        return max;
     }
 }
