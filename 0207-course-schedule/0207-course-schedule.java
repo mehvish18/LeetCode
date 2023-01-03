37d75edb54f12e2f654559d20f3bf34178b1103a
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-02 17:59:05 -0800

    Time: 9 ms (62.20%), Space: 48.3 MB (38.47%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,40 @@
+class Solution {
+    boolean [] visited;
+    int [] deg;
+    List<List<Integer>> adj = new ArrayList<>();
+    int courses;
+    List<Integer> toplogical = new ArrayList<>();
+    
+    
+    public boolean canFinish(int n, int[][] prerequisites) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        deg = new int[n];
+        visited = new boolean [n];
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            deg[prerequisites[i][0]]++;
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        for(int i=0;i<n;i++){
+            if(deg[i]==0) {
+                toplogical.add(i);
+                q.add(i);
+            }
+        }
+        
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(int i=0;i<adj.get(node).size();i++){
+                if(--deg[adj.get(node).get(i)]==0){
+                    q.add(adj.get(node).get(i));
+                    toplogical.add(adj.get(node).get(i));
+                }
+            }
+        }
+        return (toplogical.size()>=n);
+    }
+}
+
+  
