299293511d03cd0f695a1de060153d3312760e52
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-02 21:54:52 -0800

    Time: 37 ms (5.15%), Space: 44.4 MB (21.86%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,5 +1,4 @@
 class Solution {
-    //List<List<Integer>> adj = new ArrayList<>();
     int find(int [] parent,int i){
         if(parent[i]==i)
             return i;
@@ -12,13 +11,8 @@ class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int parent[] = new int[edges.length];
         for(int i=0; i<edges.length;i++){
-            //adj.add(new ArrayList<>());
             parent[i] =i;
         }
-        /*for(int i=0; i<edges.length;i++){
-            adj.get(edges[i][0]-1).add(edges[i][1]-1);
-            adj.get(edges[i][1]-1).add(edges[i][0]-1);
-        }*/
         for (int i = 0; i<edges.length; i++) {
             int x = find(parent,edges[i][0]-1);
             int y = find(parent,edges[i][1]-1);
@@ -31,6 +25,3 @@ class Solution {
         return new int[]{};
     }
 }
-/*
-1 2 3
-3 3 3*/
