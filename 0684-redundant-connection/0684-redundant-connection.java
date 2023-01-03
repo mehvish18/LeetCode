bb4dbd4b3f2e951769183d3c1ba8460721db8ebb
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-02 21:55:28 -0800

    Time: 1 ms (84.19%), Space: 44.2 MB (25.01%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -16,7 +16,6 @@ class Solution {
         for (int i = 0; i<edges.length; i++) {
             int x = find(parent,edges[i][0]-1);
             int y = find(parent,edges[i][1]-1);
-            //System.out.println(x+" "+y);
             if (x == y)
                 return edges[i];
  
