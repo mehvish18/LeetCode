d0734eb4d69c5237f64145c47a071b8f2ebbebab
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-05 14:23:05 -0800

    Time: 2417 ms (5.01%), Space: 42.6 MB (98.32%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,36 +1,55 @@
 class Solution {
+    Map<String,List<String>> adj = new HashMap<>();
+    Map<String,Boolean> visited = new HashMap<>();
+    public boolean checkDiff(String s1, String s2){
+        int cnt=0;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                cnt++;
+            }
+        }
+        if(cnt==1)
+            return true;
+        return false;
+    }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>(wordList);
-        if(!set.contains(endWord)) return 0;
-        
-        Queue<String> queue = new LinkedList<>();
-        queue.add(beginWord);
-        
-        Set<String> visited = new HashSet<>();
-        queue.add(beginWord);
-        
-        int changes = 1;
-        
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i = 0; i < size; i++){
-                String word = queue.poll();
-                if(word.equals(endWord)) return changes;
-                
-                for(int j = 0; j < word.length(); j++){
-                    for(int k = 'a'; k <= 'z'; k++){
-                        char arr[] = word.toCharArray();
-                        arr[j] = (char) k;
-                        
-                        String str = new String(arr);
-                        if(set.contains(str) && !visited.contains(str)){
-                            queue.add(str);
-                            visited.add(str);
-                        }
+        List<String> nodes = new ArrayList<>();
+        nodes.add(beginWord);
+        nodes.addAll(wordList);
+        for(String node: nodes){
+            List<String> nList = adj.getOrDefault(node,new ArrayList<String>());
+            for(String word: wordList){
+                if(!node.equals(word)){
+                    if(checkDiff(node,word)){
+                        nList.add(word);
+                        adj.put(node,nList);
                     }
                 }
             }
-            ++changes;
+        }
+        /*for(String key: adj.keySet()){
+            System.out.println(key+" "+adj.get(key));
+        }*/
+        
+        Queue<String> q = new LinkedList<>();
+        q.add(beginWord);
+        visited.put(beginWord,true);
+        Queue<Integer> c = new LinkedList<>();
+        c.add(1);
+        while(!q.isEmpty()){
+            String u = q.poll();
+            int cnt = c.poll();
+            if(adj.get(u)!=null){
+                for(String v: adj.get(u)){
+                    if(visited.get(v)==null){
+                        if(v.equals(endWord))
+                            return cnt+1;
+                        q.add(v);
+                        c.add(cnt+1);
+                        visited.put(v,true);
+                    }
+                }
+            }
         }
         return 0;
     }
