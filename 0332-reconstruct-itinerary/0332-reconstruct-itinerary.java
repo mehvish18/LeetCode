5a5def75d7ea3d4c3bb8685447db0fe62ca71503
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-05 19:55:18 -0800

    Time: 5 ms (96.41%), Space: 42.8 MB (86.66%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,53 +1,24 @@
 class Solution {
-    HashMap<String,PriorityQueue<String>>adj=new HashMap<>();
-    // HashMap<String,Integer>in=new HashMap<>();
-    // HashMap<String,Integer>out=new HashMap<>();
-    List<String>ans=new ArrayList<>();
+    Map<String,PriorityQueue<String>> adj = new HashMap<>();
+    List<String> ans = new ArrayList<>();
     public List<String> findItinerary(List<List<String>> tickets) {
-        for(List<String>s:tickets){
-            String a=s.get(0);
-            String b=s.get(1);
-            if(!adj.containsKey(a))
-                adj.put(a,new PriorityQueue<String>());
-            adj.get(a).add(b);
-            //We always strt with JFK so we dont need in and out degree
-          //  out.put(a,out.getOrDefault(a,0)+1);
-            // if(!in.containsKey(a))
-            //     in.put(a,0);
-            // in.put(b,in.getOrDefault(b,0)+1);
-            // if(!out.containsKey(b))
-            //     out.put(b,0);
+        for(int i=0;i<tickets.size();i++){
+            PriorityQueue<String> lst = adj.getOrDefault(tickets.get(i).get(0),new PriorityQueue<String>());
+            lst.add(tickets.get(i).get(1));
+            adj.put(tickets.get(i).get(0),lst);
         }
-        // String start="";
-        // for(String x:adj.keySet()){
-        //     if(out.get(x)-in.get(x)==1)
-        //         {
-        //             start=x;
-        //             break;
-        //         }
-        // }
-       dfs("JFK");
-       Collections.reverse(ans);
-       return ans;
-        
+        dfs("JFK");
+        Collections.reverse(ans);
+        return ans;
     }
-     void dfs(String node){
-     
-        if(adj.containsKey(node)){
-            while(adj.get(node).size()>0){
-              PriorityQueue<String>a=adj.get(node);
-                //int n=a.size();
-                String x=a.poll();
-                // a.remove(n-1);
-                dfs(x);
-             
+    public void dfs(String source){
+        PriorityQueue<String> q = adj.get(source);
+        if(q!=null){
+            while(q.size()>0){
+                String v = q.poll();
+                dfs(v);
             }
-            
         }
-
-        ans.add(node);
-
-
-        return;
+        ans.add(source);
     }
 }
