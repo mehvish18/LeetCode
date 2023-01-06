77e376b15eb0a185e671ed51c073ab9671778968
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-05 16:09:07 -0800

    Time: 12 ms (100.00%), Space: 43.4 MB (93.19%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,52 +1,42 @@
 class Solution {
-
+    public int binaryBfs(Set<String> begin, Set<String> end, Set<String> dictionary, int level){
+       /* System.out.println(begin);
+        System.out.println(end);
+        System.out.println(dictionary);
+        System.out.println(level+"\n");*/
+        if(begin.size()==0||end.size()==0)
+            return 0;
+        level++;
+        dictionary.removeAll(begin);
+        Set<String> newSet = new HashSet<>();
+        for(String s:begin){
+            char [] oldword = s.toCharArray();
+            for(int i=0;i<oldword.length;i++){
+                char oldchar = oldword[i];
+                for(char c='a';c<='z';c++){
+                    oldword[i]=c;
+                    String newWord = new String(oldword);
+                    if(!dictionary.contains(newWord))
+                        continue;
+                    if(end.contains(newWord))
+                        return level;
+                    newSet.add(newWord);
+                }
+                oldword[i]=oldchar;
+            }
+        }
+        return (newSet.size()>end.size())?binaryBfs(end,newSet,dictionary,level):binaryBfs(newSet,end,dictionary,level);
+    }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         if(beginWord.equals(endWord) || !wordList.contains(endWord))
             return 0;
-
-        Set<String> beginSet = new HashSet<>();
-        beginSet.add(beginWord);
-
-        Set<String> endSet = new HashSet<>();
-        endSet.add(endWord);
-
-        Set<String>  dictionary = new HashSet<>(wordList);
-
-        return binaryBfs(beginSet, endSet, dictionary, 1);
-    }
-
-    private int binaryBfs(Set<String> beginSet, Set<String> endSet, Set<String> dictionary, int level) {
-        if(beginSet.isEmpty() || endSet.isEmpty())
-            return 0;
-
-        level++;
-        dictionary.removeAll(beginSet);
-        Set<String> nextSet = new HashSet<>();
-
-        for(String word: beginSet){
-            char[] oldWord = word.toCharArray();
-            for(int i=0;i<oldWord.length;i++){
-                char oldChar = oldWord[i];
-
-                for(char c='a';c<='z';c++){
-                    oldWord[i] = c;
-
-                    String newWord = new String(oldWord);
-
-                    if(!dictionary.contains(newWord))
-                        continue;
-
-                    if(endSet.contains(newWord))
-                        return level;
-
-                    nextSet.add(newWord);
-                }
-
-                oldWord[i] = oldChar;
-            }
-        }
-
-        return nextSet.size() > endSet.size() ? binaryBfs(endSet, nextSet, dictionary, level) :
-                binaryBfs(nextSet, endSet, dictionary, level);
+        Set<String> begin = new HashSet<>();
+        begin.add(beginWord);
+        
+        Set<String> end = new HashSet<>();
+        end.add(endWord);
+        
+        Set<String> dictionary = new HashSet<>(wordList);
+        return binaryBfs(begin,end,dictionary,1);
     }
 }
