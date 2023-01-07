57a7101aa681b9943d503eab3797ee0a0069c3c6
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-07 12:09:18 -0800

    Time: 57 ms (92.05%), Space: 47.1 MB (90.28%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,54 @@
+class Solution {
+    int v;
+    int [][] graph;
+    int [] parent;
+    public int minKey(int [] key, boolean [] inMST){
+        int m = Integer.MAX_VALUE;
+        int mIndex = -1;
+        for(int i=0;i<v;i++){
+           if(!inMST[i] && key[i]<=m){
+               m=key[i];
+               mIndex = i;
+           }
+        }
+        return mIndex;
+    }
+    public void prims(){  
+        int [] key = new int[v];
+        boolean [] inMst = new boolean[v];
+        Arrays.fill(key, Integer.MAX_VALUE);
+        key[0]=0;
+        parent[0]=-1;
+        for(int i=0;i<v-1;i++){
+            int u = minKey(key,inMst);
+            inMst[u] = true;
+            for(int j=0;j<v;j++){
+                if(graph[u][j]!=0 && !inMst[j] && graph[u][j]<key[j]){
+                    key[j]=graph[u][j];
+                    parent[j]=u;
+                }
+            }
+        }
+    }
+    public int minCostConnectPoints(int[][] points) {
+        v = points.length;
+        parent = new int[v];
+        graph = new int[v][v];
+        for(int i=0;i<v;i++){
+            for(int j=0; j<v;j++){
+                if(i==j)
+                    graph[i][j]=0;
+                else
+                    graph[i][j] = Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
+                //System.out.println(i+" "+j+" "+graph[i][j]);
+            }
+        }
+        prims();
+        int cost=0;
+        //System.out.println(Arrays.toString(parent));
+        for(int i=1;i<v;i++){
+            cost += graph[i][parent[i]];
+        }
+        return cost;
+    }
+}
