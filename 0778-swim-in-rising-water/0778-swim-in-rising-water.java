ec20386f6a60a6682c8e6176b57d80bc0be9795d
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-08 15:45:19 -0800

    Time: 22 ms (53.06%), Space: 42.6 MB (64.89%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,36 +1,32 @@
 class Solution {
-    public int swimInWater(int[][] g) {
-        int m = g.length, n = g[0].length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[] p1, int[] p2){
-                return g[p1[0]][p1[1]]-g[p2[0]][p2[1]];
+    public int swimInWater(int[][] grid) {
+        int [][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+        int m = grid.length;
+        int n = grid[0].length;
+        PriorityQueue<int []> pq = new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int p1[], int p2[]){
+                return grid[p1[0]][p1[1]]-grid[p2[0]][p2[1]];
             }
         });
-        int[][] seen = new int[m][n];
+        boolean [][] seen = new boolean[m][n];
         pq.add(new int[]{0,0});
-        int max = g[0][0];
+        int max = grid[0][0];
         while(true){
-            int[] t = pq.remove();
-            if(seen[t[0]][t[1]] == 1) continue;
-            seen[t[0]][t[1]] = 1;
-            max = Math.max(max, g[t[0]][t[1]]);
-            if(t[0] == m-1 && t[1] == n-1) break;
-            for(int[] child : children(g, t[0], t[1]))
-                pq.add(child);
+            int [] t = pq.remove();
+            if(seen[t[0]][t[1]])
+                continue;
+            seen[t[0]][t[1]]=true;
+            max = Math.max(max,grid[t[0]][t[1]]);
+            if(t[0]==m-1 && t[1]==n-1)
+                break;
+            for(int i=0;i<4;i++){
+                int x  = t[0]+dir[i][0];
+                int y  = t[1]+dir[i][1];
+                if(x>=0 && y>=0 && x<m && y<n){
+                    pq.add(new int[]{x,y});
+                }
+            }   
         }
         return max;
-    }
-    private List<int[]> children(int[][] g, int i, int j){
-        int m = g.length, n = g[0].length;
-        List<int[]> list = new ArrayList<>();
-        if(i+1 < m)
-            list.add(new int[]{i+1, j});
-        if(i-1 >= 0)
-            list.add(new int[]{i-1, j});
-        if(j+1 < n)
-            list.add(new int[]{i, j+1});
-        if(j-1 >= 0)
-            list.add(new int[]{i, j-1});
-        return list;
     }
 }
