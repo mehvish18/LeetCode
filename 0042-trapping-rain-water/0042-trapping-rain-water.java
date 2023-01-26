99754952fb729e768176a71665979e0ad9b9029a
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-01-25 17:27:50 -0800

    Time: 1 ms (98.84%), Space: 49.4 MB (9.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,40 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-        int l=0,r=height.length-1;
-        int ans=0;
-        int lastH=0;
-        boolean left=true;
-        int c=0;
-        while(l<r){
-            int hgt = Math.min(height[r],height[l]);
-            int width = r-l-1;
-            //System.out.println("l: "+l+" r"+r);
-            
-            if(c>0){
-                int h;
-                if(left)
-                    h=Math.min(height[l],lastH);
-                else
-                    h=Math.min(height[r],lastH);
-                //System.out.println(h);
-                ans = ans-h;
-                
+        int left=0, right=height.length-1;
+        int leftbar = height[left], rightbar=height[right];
+        int rain = 0;
+        while(left < right){
+            if(leftbar<rightbar){
+                if(height[left]>leftbar)
+                    leftbar = height[left];
+                else{
+                    rain += leftbar - height[left];
+                    left++;
+                }
             }
-            if(hgt>lastH){
-                ans = ans+(hgt-lastH)*width;
-                lastH = hgt;
-                c++;//System.out.println("c: "+c);
-                
-            }
-            if(height[l]<height[r]){
-                left=true;
-                l++;
-            }
-            else {
-                left=false;
-                r--;
+            else{
+                if(height[right]>rightbar)
+                    rightbar = height[right];
+                else{
+                    rain += rightbar - height[right];
+                    right--;
+                }
             }
         }
-        return ans;
+        return rain;
     }
 }
