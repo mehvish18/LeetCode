2262524c975e39cd4b2af5bac67b6f5a422bf86e
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-02-01 18:58:49 -0800

    Time: 26 ms (39.56%), Space: 42.5 MB (51.64%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,44 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int len1 = s1.length(), len2 = s2.length();
-        if (len1 > len2) return false;
-        
-        int[] count = new int[26];
-        for (int i = 0; i < len1; i++) {
-            count[s1.charAt(i) - 'a']++;
-            count[s2.charAt(i) - 'a']--;
+        if(s2.length()<s1.length())
+            return false;
+        Map<Character,Integer> mp = new HashMap<>();
+        int k=s1.length(),co=0;
+        for(int i=0;i<s1.length();i++){
+            mp.put(s1.charAt(i),mp.getOrDefault(s1.charAt(i),0)+1);
         }
-        if (allZero(count)) return true;
-        
-        for (int i = len1; i < len2; i++) {
-            count[s2.charAt(i) - 'a']--;
-            count[s2.charAt(i - len1) - 'a']++;
-            if (allZero(count)) return true;
+        int l=0;
+        for(int i=0;i<k;i++){
+            char c = s2.charAt(i);
+            if(mp.get(c)!=null){    
+                mp.put(c,mp.get(c)-1);
+                if(mp.get(c)==0)
+                    co++;
+            }
         }
-        
+        //System.out.println(mp.size());
+        if(co==mp.size())
+            return true;
+        for(int i=k;i<s2.length();i++){
+            char c = s2.charAt(i);
+            //System.out.println(s2.charAt(i-k)+" "+c);
+            //System.out.println("before co "+co);
+            if(mp.get(s2.charAt(i-k))!=null){
+                if(mp.get(s2.charAt(i-k))==0)
+                    co--;
+                mp.put(s2.charAt(i-k),mp.get(s2.charAt(i-k))+1);
+                //System.out.println("yo "+s2.charAt(i-k)+" "+mp.get(s2.charAt(i-k))+" "+counterMap.get(s2.charAt(i-k)));
+            }
+            if(mp.get(c)!=null){ 
+                mp.put(c,mp.get(c)-1);
+                //System.out.println("hello "+c+" "+counterMap.get(c)+" "+mp.get(c));
+                if(mp.get(c)==0)
+                    co++;
+            }
+            //System.out.println("after co "+co);
+            if(co==mp.size())
+                return true;
+        }
         return false;
-    }
-    
-    private boolean allZero(int[] count) {
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) return false;
-        }
-        return true;
     }
 }
