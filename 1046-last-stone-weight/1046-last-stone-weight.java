57d2494fa87354b9baf1c7a523caa524d45c8bde
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-02-04 02:43:43 -0800

    Time: 1 ms (98.68%), Space: 39.9 MB (61.37%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -7,14 +7,11 @@ class Solution {
         while(pq.size()>1){
                 int y = pq.poll();
                 int x = pq.poll();
-                System.out.println(y+" x "+x);
                 if(y!=x){
-                    System.out.println(y-x);
                     pq.add(y-x);
                 }
     
         }
-        System.out.println(pq.size());
         if(pq.size()==0)
             return 0;
         return pq.peek();
