8b116739e8224e8d8a8e5937980d885b31c36681
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-02-07 01:47:53 -0800

    Time: 37 ms (63.93%), Space: 50.2 MB (35.74%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if((i==0) || (nums[i]!=nums[i-1])){
-                int sum = 0-nums[i];
-                int l = i+1;
-                int h = nums.length-1;
-                while(l<h){
-                    if(nums[l]+nums[h]==sum){
-                        List<Integer> a = new ArrayList<>();
-                        a.add(nums[i]);
-                        a.add(nums[l]);
-                        a.add(nums[h]);
-                        ans.add(a);
-                        while (l < h && nums[l] == nums[l+1]) l++;
-                        while (l < h && nums[h] == nums[h-1]) h--;
-                        l++;
-                        h--;
-                    }
-                    else if(nums[l]+nums[h]>sum)
-                        h--;
-                    else
-                        l++;
-                        
-                }
-            }
-        }
+       Arrays.sort(nums);
+       //System.out.println(Arrays.toString(nums));
+       List<List<Integer>> ans = new ArrayList<>();
+       for(int i=0;i<nums.length-1;i++){
+           int sum = -nums[i];
+           int l = i+1;
+           int h = nums.length-1;
+           if(i!=0 && nums[i]==nums[i-1])
+              continue;
+           while(l<h){
+               if(nums[l]+nums[h]==sum){
+                   List<Integer> a = new ArrayList<>();
+                   a.add(nums[i]);
+                   a.add(nums[h]);
+                   a.add(nums[l]);
+                   ans.add(a); 
+                   while(l<h && nums[l+1]==nums[l])
+                    l++;
+                   while(l<h && nums[h-1]==nums[h])
+                    h--;
+                   l++;
+                   h--;
+               }
+               else if(nums[l]+nums[h]<sum)
+                   l++;
+               else 
+                   h--;
+           }
+       }
         return ans;
     }
 }
+//[-5, -5, -4, -4, -4, -2, -2, -2, 0, 0, 0, 1, 1, 3, 4, 4]
+//-5 4 1, -4 3 1, -4 40, -2,-2,4,
+    
+//-2 0  1
