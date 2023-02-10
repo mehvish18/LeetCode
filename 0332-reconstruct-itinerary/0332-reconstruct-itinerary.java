ffe5da6aeeb8bfb17f593330cf37f56c92610826
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-02-09 21:30:36 -0800

    Time: 7 ms (57.03%), Space: 42.9 MB (73.17%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -2,6 +2,7 @@ class Solution {
     Map<String,PriorityQueue<String>> adj = new HashMap<>();
     List<String> ans = new ArrayList<>();
     public List<String> findItinerary(List<List<String>> tickets) {
+        System.out.println(ans.size());
         for(int i=0;i<tickets.size();i++){
             PriorityQueue<String> lst = adj.getOrDefault(tickets.get(i).get(0),new PriorityQueue<String>());
             lst.add(tickets.get(i).get(1));
