36d0fa29b01f76a0c07cb66d07134ec6dcdd3708
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-04-13 23:08:11 -0700

    Time: 1 ms (99.38%), Space: 42.8 MB (87.47%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,36 @@
 class Solution {
     public int trap(int[] height) {
-        int left=0, right=height.length-1;
-        int leftbar = height[left], rightbar=height[right];
-        int rain = 0;
-        while(left < right){
-            if(leftbar<rightbar){
-                if(height[left]>leftbar)
-                    leftbar = height[left];
-                else{
-                    rain += leftbar - height[left];
-                    left++;
-                }
+        int l=0, r=height.length-1;
+        int area = 0;
+        int currMin = 0;
+        //System.out.println("l "+l+" r "+r+" area " +area+ " cm "+currMin);
+        while(l<r){
+            area = area - Math.min(currMin,Math.min(height[l],height[r]));
+            if(currMin<Math.min(height[l],height[r])){ 
+                area = area +(Math.min(height[l],height[r]) - currMin)*(r-l-1);
+                currMin = Math.min(height[l],height[r]);  
             }
-            else{
-                if(height[right]>rightbar)
-                    rightbar = height[right];
-                else{
-                    rain += rightbar - height[right];
-                    right--;
-                }
-            }
+            //System.out.println(height[l]+" "+height[r]);
+             //System.out.println("l "+l+" r "+r+" area " +area+ " cm "+currMin);
+            if(height[l]<=height[r])
+                l++;
+            else
+                r--;
+           
         }
-        return rain;
+        return area;
+        
     }
 }
+
+
+
+
+/*
+0,1,0,2,1,0,1,3,2,1, 2, 1
+0 1 2 3 4 5 6 7 8 9 10 11
+
+
+
+0+9-0-0-1+6-1-1-0-1-2-1-2 = 9-9+6 = 6
+*/
