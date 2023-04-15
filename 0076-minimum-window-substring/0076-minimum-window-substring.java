ab35e7b3c6ba221be86ce17fd09662cb6c0be0e3
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-04-15 01:55:19 -0700

    Time: 24 ms (41.60%), Space: 42.2 MB (97.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,47 +1,68 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int l = 0,r;
-        int co=0;
-        int min=Integer.MAX_VALUE;
-        String minS="";
-        Map<Character, Integer> mp = new HashMap<>();
-        for(int i=0;i<t.length();i++){
-            mp.put(t.charAt(i),mp.getOrDefault(t.charAt(i),0)+1);
+        String str="";
+        if(t.length()>s.length())
+            return str;
+        int i=0,j=0,min=Integer.MAX_VALUE,lc=0;
+        Map<Character,Integer> mp = new HashMap<>();
+        for(i=0;i<t.length();i++){
+            char ch = t.charAt(i);
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
         }
-        for(r=0;r<s.length();r++){
-            char c = s.charAt(r);
-            if(mp.get(c)!=null){
-                    l=r;
-                    break;
+        i=0;
+        while(i<s.length() && mp.get(s.charAt(i))==null){
+            j++;
+            i++;
+        }
+        for(;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(mp.get(ch)!=null){
+                mp.put(ch,mp.get(ch)-1);
+            
+            if(mp.get(ch)==0){
+                lc++;
             }
-        }
-        for(;r<s.length();r++){
-           char c = s.charAt(r);
-           if(mp.get(c)!=null){
-               mp.put(c,mp.get(c)-1);
-               if(mp.get(c)==0){
-                   co++;
-               }
-               while(mp.get(c)<=0){ 
-                   if(mp.get(s.charAt(l))!=null){ 
-                    if((mp.get(s.charAt(l))+1)>0){
+            while(mp.get(ch)<=0){
+                char c = s.charAt(j);
+                if(mp.get(c)!=null){
+                    if(mp.get(c)+1>0){
                         break;
                     }
-                    mp.put(s.charAt(l),mp.get(s.charAt(l))+1);
-                   }
-                   l++;
-               }
-           }
-           int d = co;
-           
-           if(co==mp.size()){
-               if(min>(r-l+1)){
-                   min=r-l+1;
-                   minS=s.substring(l,r+1);
-               }
-           }
-           
+                    mp.put(c,mp.get(c)+1);  
+                }
+                j++;
+                
+            }
+            }
+            if(lc==mp.size()){
+                if((i-j+1)<min){
+                    min = i-j+1;
+                    str = s.substring(j,i+1);
+                }
+            }    
         }
-        return minS;
+        return str;
     }
 }
+
+/*
+s "aaaaaaaaaaaabbbbbcdd"
+t "abcdd"
+A:1
+B:1
+C:1
+D:2
+w= 4
+
+w=1
+A:0
+B:1
+C:1
+D:2
+
+i=1
+j=1
+
+
+
+*/
