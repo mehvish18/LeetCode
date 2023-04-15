61d9407791d916c0cef649962c5ec2f1a9f722a4
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-04-14 18:45:44 -0700

    Time: 32 ms (19.37%), Space: 42 MB (94.06%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,18 +1,35 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l=0,r=0,max=0,mostFreqLetter=0;
-        int lettersChanged=0;
-        int []freq = new int[26];
-        for(r=0;r<s.length();r++){
-            freq[s.charAt(r)-'A']++;
-            mostFreqLetter = Math.max(mostFreqLetter,freq[s.charAt(r)-'A']);
-            lettersChanged = r-l+1-mostFreqLetter;
-            if(lettersChanged>k){
-                freq[s.charAt(l)-'A']--;
-                l++;
+       Map<Character,Integer> mp = new HashMap<>();
+       int i=0,j=0,max=0;
+       char maxChar;
+       int mc=0;
+       while(i<s.length()){
+            char ch = s.charAt(i);
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
+            if(mp.get(ch)>mc){
+                mc = mp.get(ch);
+                maxChar = ch;
             }
-            max = Math.max(max,(r-l+1));
+            if(i-j+1-mc-k>0){
+                mp.put(s.charAt(j),mp.get(s.charAt(j))-1);
+                j++;
+            }
+            max = Math.max(max,(i-j+1));
+            i++;
         }
         return max;
     }
 }
+/*
+k=2
+
+AA ABBC
+
+
+
+
+
+
+
+*/
