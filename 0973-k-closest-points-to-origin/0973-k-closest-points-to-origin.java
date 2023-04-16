a1a1dc2400eb55930a51d26b5d3268bd8432a2be
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-04-16 15:56:51 -0700

    Time: 46 ms (29.99%), Space: 51 MB (49.20%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,42 +1,23 @@
 class Solution {
-    class Point{
-        public int x;
-        public int y;
-        public double val;
-
-    }
     public int[][] kClosest(int[][] points, int k) {
         int[][] ans = new int[k][2];
-        PriorityQueue<Point> pq = new PriorityQueue<Point>(1, new Comparator<Point>() {
-            public int compare(Point p1, Point p2) {
-                if(p1.val==p2.val)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(k, new Comparator<int[]>(){
+            public int compare(int[] p1, int[] p2){
+                double d1 = Math.sqrt(Math.pow(p1[0],2)+Math.pow(p1[1],2));
+                double d2 = Math.sqrt(Math.pow(p2[0],2)+Math.pow(p2[1],2));
+                if(d1==d2)
                     return 0;
-                else if(p1.val<p2.val)
+                else if(d1<d2)
                     return -1;
-                else
-                    return 1;
-                    
+                return 1;
             }
         });
         for(int i=0;i<points.length;i++){
-            double d = Math.sqrt(Math.pow(points[i][0],2)+Math.pow(points[i][1],2));
-            Point p = new Point();
-            p.x = points[i][0];
-            p.y = points[i][1];
-            p.val = d;
-            pq.add(p);    
+            pq.add(points[i]);
         }
-        /*for(int i=0;i<points.length;i++){
-            Point p = pq.poll();
-            System.out.println(p.x+" "+p.y+" "+p.val);  
-        }*/
-        
         for(int i=0;i<k;i++){
-            Point p = pq.poll();
-            ans[i][0] = p.x;
-            ans[i][1] = p.y;
+            ans[i]=pq.poll();
         }
         return ans;
     }
-    
 }
