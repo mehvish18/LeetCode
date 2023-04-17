317f6eba585c51638185843452f2ba50a135725f
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-04-17 01:36:23 -0700

    Time: 54 ms (13.79%), Space: 44.2 MB (31.12%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -19,77 +19,38 @@ class Solution {
             pqEle[1] = mp.get(a);
             pq.add(pqEle);
         }
-        int i=0;
+        int time=0;
         while(pq.size()>0){
-            //System.out.println(" pq"+pq.size());
             int pqEle[] = pq.poll();
-            //System.out.println(Arrays.toString(pqEle));
-            if(index.get(pqEle[0])==null || (i-index.get(pqEle[0])>n)){
-                index.put(pqEle[0],i);
-                pqEle[1]=pqEle[1]-1;
-                if(pqEle[1]>0)
-                    pq.add(pqEle);
+            if(pqEle!=null)
+                time++;
+            if(pq.size()==0 && pqEle[1]-1==0)
+                break;
+            List<int[]> lst = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                int [] ElementsLeftToSelect = pq.poll();
+                lst.add(ElementsLeftToSelect);
+                time++;
+                if(pq.size()==0 && pqEle[1]-1==0)
+                        break; 
             }
-            else if(i-index.get(pqEle[0])<=n){ 
-                List<int[]> lst = new ArrayList<>();
-                while(index.get(pqEle[0])!=null && i-index.get(pqEle[0])<=n){
-                    //System.out.println(pqEle[0]+" "+index.get(pqEle[0]));
-                    lst.add(pqEle);
-                    if(pq.peek()==null)
-                        break;
-                    pqEle = pq.poll();
-                }
-                for(int j=0;j<lst.size();j++){
-                    pq.add(lst.get(j));
-                } 
-                if(index.get(pqEle[0])==null || i-index.get(pqEle[0])>n){
-                    index.put(pqEle[0],i);
-                    pqEle[1]=pqEle[1]-1;
-                    if(pqEle[1]>0)
-                        pq.add(pqEle);
+            if(pqEle[1]>1){
+                pqEle[1]=pqEle[1]-1; 
+                pq.add(pqEle);
+            }
+            for(int[] ele : lst){
+                if(ele!=null) {
+                        if(ele[1]>1){
+                            ele[1]=ele[1]-1; 
+                            pq.add(ele);
+                        }
                 }
             }
-            i++;
         }
-        return i;
+        return time;
         
     }
 }
 
 
-/*
-"A","A","A","B","B","B" n=2
 
-
-A:0,2
-B:0,2
-
-A B idle A B idle A B
-
-
-
-
-["A","A","A","A","A","A","B","C","D","E","F","G"]
-n=2
-
-
-A: 4
-D: 1
-E: 1
-F: 1
-G: 1
-
-A i=3
-B
-
-A B C A
-
-A:0 B:1 C:2
-
-
-
-A B C A D E A F G A id id A id id A
-
-
-
-*/
