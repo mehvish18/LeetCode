b4d4dc837763d5c786cf23fcbba9c4eb4601b9be
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-04-18 01:42:28 -0700

    Time: 0 ms (100.00%), Space: 40.1 MB (48.66%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,24 @@
 class Solution {
-
-    public int profit(int [] nums, int start,int end){
-        //System.out.println(start+" "+end);
-        int [] dp = new int[nums.length];
-        dp[start]= nums[start];
-        dp[start+1]= Math.max(nums[start],nums[start+1]);
-        for(int i=start+2;i<=end;i++){
-            dp[i] = Math.max(dp[i-1],nums[i]+dp[i-2]);
+    public int getdp(int[] nums, int strt ,int end){
+        int n= end-strt+1;
+        int dp[] = new int[n+1];
+        dp[0]=0;
+        dp[1]=nums[strt];
+        dp[2]=Math.max(nums[strt],nums[strt+1]);
+        for(int i=3;i<=n;i++){
+            dp[i] = Math.max(dp[i-2]+nums[strt+i-1],dp[i-1]);
         }
-        return dp[end];
+        return dp[n];
     }
     public int rob(int[] nums) {
+        int dp[] = new int[nums.length];
         if(nums.length==1)
             return nums[0];
         else if(nums.length==2)
             return Math.max(nums[0],nums[1]);
-        int a = profit(nums,0,nums.length-2);
-        int b = profit(nums,1,nums.length-1);
-        return Math.max(a,b);
+        int n= getdp(nums,0,nums.length-2);
+        int m= getdp(nums,1,nums.length-1);
+        
+        return Math.max(n,m);
     }
 }
