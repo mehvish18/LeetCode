589091c692c1425d8f02cb8634c39887a0a3e586
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-05-27 00:48:05 -0700

    Time: 117 ms (89.80%), Space: 68.2 MB (12.99%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,43 +1,71 @@
 class MedianFinder {
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>();;
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-    
+    PriorityQueue<Integer>left;
+    PriorityQueue<Integer>right;
     public MedianFinder() {
-     
+      left = new PriorityQueue(Collections.reverseOrder());
+      right = new PriorityQueue(); 
     }
     
     public void addNum(int num) {
-       maxHeap.add(num);
-       if(maxHeap.size()-minHeap.size()>1 || (minHeap.peek()!=null && minHeap.peek()<maxHeap.peek())){
-           minHeap.add(maxHeap.poll());
-       }
-       if(minHeap.size()-maxHeap.size()>1){
-           maxHeap.add(minHeap.poll());
-       } 
+        if(right.size() > 0 && num >= right.peek()){
+            right.add(num);
+        }
+        else{
+           left.add(num);
+        }
+        if(right.size() - left.size() == 2 ){
+            left.add(right.remove());
+        }
+        else if(left.size() - right.size() == 2){
+            right.add(left.remove());
+        }
     }
     
     public double findMedian() {
-        Integer minHead = minHeap.peek();
-        Integer maxHead = maxHeap.peek();
-        if(minHeap.size()==maxHeap.size()){
-            return ((double)(minHead+maxHead)/2);
+        double median = 0;
+        if(left.size() > right.size()){
+            median = (double)left.peek();
         }
-        else if(minHeap.size()>maxHeap.size()){
-            return minHead;
+        else if(left.size() == right.size()){
+            median = (double)(left.peek() + right.peek())/2;
         }
-        else {
-            return maxHead;
+        else{
+            median = (double)right.peek();
         }
+        return median;
     }
 }
-
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder obj = new MedianFinder();
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
- */
 
 
- 
+
+
+1 2 3 2 4 
     
+1 2 2 3 4
+    
+    
+1 1 2 2 2 3 4
+    
+1
+1 2
+1 2,3
+2,1 2,3
+2,1,2 2,3
+2,1,2 2,3,4
+2,1,2,1 2,3,4
+    
+    
+    
+10 2 0 -3 3
+      
+    
+2 10
+2,0 10
+2,0,-3 10 => 0,-3 2,10
+2,0,-3 3,10
+ */
