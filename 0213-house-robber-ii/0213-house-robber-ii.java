7a1cee8d8a1cf278d71343f4d9075b04929926f5
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-06-09 09:46:15 -0700

    Time: 0 ms (100.00%), Space: 40.5 MB (12.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,38 @@
 class Solution {
-    public int getdp(int[] nums, int strt ,int end){
-        int n= end-strt+1;
-        int dp[] = new int[n+1];
-        dp[0]=0;
-        dp[1]=nums[strt];
-        dp[2]=Math.max(nums[strt],nums[strt+1]);
-        for(int i=3;i<=n;i++){
-            dp[i] = Math.max(dp[i-2]+nums[strt+i-1],dp[i-1]);
+    public int constructDpTable(int [] nums, int start, int end, int n){
+        int dp[] = new int[n-1];
+        dp[0]=nums[start];
+        dp[1]=Math.max(nums[start],nums[start+1]);
+        int c=2;
+        for(int i=start+2;i<end;i++){
+            dp[c] = Math.max(nums[i]+dp[c-2],dp[c-1]);
+            c++;
         }
-        return dp[n];
+        return dp[n-2];
     }
     public int rob(int[] nums) {
-        int dp[] = new int[nums.length];
-        if(nums.length==1)
+        int n = nums.length;
+        if(n==1)
             return nums[0];
-        else if(nums.length==2)
+        else if(n==2)
             return Math.max(nums[0],nums[1]);
-        int n= getdp(nums,0,nums.length-2);
-        int m= getdp(nums,1,nums.length-1);
+        int val1 = constructDpTable(nums, 0, n-1, n);
+        int val2 = constructDpTable(nums, 1, n, n);
+        return Math.max(val1,val2);
         
-        return Math.max(n,m);
     }
 }
+
+/*
+[1,2,3]
+
+n=3 
+
+0 1
+1 2 
+
+0 1 2
+2 3
+
+
+*/
