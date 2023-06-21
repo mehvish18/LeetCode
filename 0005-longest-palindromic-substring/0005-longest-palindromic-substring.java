a9583032dee4f160f019c5e40267010be6fe931e
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-06-21 02:43:43 -0700

    Time: 436 ms (10.54%), Space: 54.9 MB (5.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,54 +1,54 @@
 class Solution {
     public String longestPalindrome(String s) {
+        String rev = "";
+        int max = 0;
+        int idx = 0;
         int n = s.length();
-        int dp[][] = new int[n][n];
-        int max=1, idx=0;
-        for(int k=0;k<n;k++){
-            dp[k][k] = 1;
+        for(int i=n-1;i>=0;i--){
+            rev = rev + s.charAt(i);
         }
-        for(int k=0;k<n-1;k++){
-            if(s.charAt(k)==s.charAt(k+1)){
-                dp[k][k+1] = 1;
-                max=2;
-                idx=k;                                            
-            }
+        int dp[][] = new int[n+1][n+1];
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
         }
-        for(int k=3;k<=n;k++){
-            for(int i=0;i<=n-k;i++){
-                int j = i+k-1;
-                if(i>j)
-                    continue;
-                    
-                if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==1){
-                    dp[i][j] = 1;
-                    if(k>max){
-                        max=k;
-                        idx=i;                                            
+        
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=n;j++){
+                if(s.charAt(i-1)==rev.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    if(i-dp[i][j]== n-j){
+                        if(dp[i][j]>max){
+                            max = dp[i][j];
+                            idx = i;
+                        }
                     }
                 }
             }
         }
-        return s.substring(idx,idx+max);
+        return s.substring(idx-max,idx);
     }
 }
 
 /*
+babad
+01234
 
-k=3
-i=0, j = 2
+dabab
+01234
 
-i=1, j=3
+n-1-i + dp [i][j] = j
 
+
+  0 1 2 3 4 5
+0 0 0 0 0 0 0
+1 0 0 0 1 0.1
+2 0 0  
+3 0  
+4 0
+5 0       
 
 cbbd
-0123
-
-
-  0 1 2 3
-0 1 0 0 0
-1   1 1 0
-2     1 0
-3       1
 
 
 
