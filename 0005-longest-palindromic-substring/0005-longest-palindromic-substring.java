9f902059b09fa7f0d3d750c2b811d1067598baaf
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-07-02 22:50:17 -0700

    Time: 434 ms (10.21%), Space: 55 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,56 +1,50 @@
 class Solution {
     public String longestPalindrome(String s) {
         String rev = "";
-        int max = 0;
-        int idx = 0;
         int n = s.length();
-        for(int i=n-1;i>=0;i--){
-            rev = rev + s.charAt(i);
-        }
+    
+        int max = 1, idx=1;
+        String ans = "";
+        for(int i=n-1;i>=0;i--)
+            rev += s.charAt(i);
         int dp[][] = new int[n+1][n+1];
-        for(int i=0;i<n;i++){
-            dp[i][0]=0;
-            dp[0][i]=0;
-        }
-        
-        for(int i=1; i<=n;i++){
-            for(int j=1; j<=n;j++){
-                if(s.charAt(i-1)==rev.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1]+1;
-                    if(i-dp[i][j]== n-j){
-                        if(dp[i][j]>max){
-                            max = dp[i][j];
-                            idx = i;
-                        }
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0)
+                    dp[i][j]=0;
+                else if(s.charAt(i-1)==rev.charAt(j-1)){
+                    dp[i][j] = 1 +dp[i-1][j-1];
+                    if(dp[i][j]>max && (i-dp[i][j] == n-j)){
+                        max = dp[i][j];
+                        idx = i;
                     }
                 }
+                else
+                    dp[i][j]=0;
+                    
             }
         }
-        return s.substring(idx-max,idx);
+        ans = s.substring(idx-max,idx);
+        return ans;
     }
 }
 
 /*
+
 babad
 01234
 
 dabab
 01234
 
-n-1-i + dp [i][j] = j
 
-
-  0 1 2 3 4 5
-0 0 0 0 0 0 0
-1 0 0 0 1 0.1
-2 0 0  
-3 0  
+  0 1 2 3 4 5 
+0 0 0 0 0 0 0 
+1 0 0 0 1 0 1 
+2 0 0 1 0 1 0 
+3 0
 4 0
-5 0       
+5 0
 
-cbbd
-
-
-
-
+The dp[][] array is a 2-dimensional matrix where each cell dp[i][j] represents the length of the longest common substring ending at index i in the first string and index j in the second string.
 */
