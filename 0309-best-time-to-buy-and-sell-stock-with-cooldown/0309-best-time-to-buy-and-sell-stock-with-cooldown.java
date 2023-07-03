2680d2de00fe68a0bd3492d2a3e6b4825ca3f846
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-07-03 10:17:40 -0700

    Time: 1 ms (83.27%), Space: 40.6 MB (72.96%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,24 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length <2 ) {
+        int n = prices.length;
+        if(n==1)
             return 0;
-        }
-        if (prices.length == 2) {
-            return Math.max(prices[1] - prices[0], 0);
-        }
-        int profit = 0;
-        int[][] dp = new int[prices.length][2];
-        //dp[i][0]: no share at hand at the end of day i
-        //dp[i][1]: has share at the end of day i
-        dp[0][1]  = -prices[0];
-        dp[1][0] = Math.max(0, prices[1] - prices[0]);
-        dp[1][1] = Math.max(-prices[0], -prices[1]);
+        else if(n==2)
+            return Math.max(0, prices[1]-prices[0]);
         
-        for (int i=2; i<= prices.length-1; i++) { //len >= 3
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-2][0]-prices[i]);//3 days to complete a transaction
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            profit = Math.max(profit, Math.max(dp[i][0], dp[i][1]));
+        int dp[][] = new int[n][2];
+        dp[0][1] = -prices[0];
+        dp[1][1]= Math.max(-prices[1], -prices[0]);
+        dp[1][0] = Math.max(prices[1]-prices[0], 0);
+        int max=0;
+        for(int i=2;i<n;i++){
+            dp[i][0] = Math.max(prices[i]+dp[i-1][1], dp[i-1][0]);
+            dp[i][1] = Math.max(dp[i-2][0]-prices[i], dp[i-1][1]);
+            int profit = Math.max(dp[i][0],dp[i][1]);
+            max = Math.max(profit,max);
         }
-        return profit;
+        return max;
     }
 }
