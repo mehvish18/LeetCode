99bb53b25d69594371cd8c13a532fc28278ae197
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-07-03 01:52:55 -0700

    Time: 1 ms (83.27%), Space: 41 MB (41.13%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,26 +1,24 @@
 class Solution {
-    int [] price;
-    Map<String, Integer> dp = new HashMap<>();
-    public int buySell(int index, boolean buying){
-        if(index>=price.length)
-            return 0;
-        if(dp.get(index+" "+buying)!=null)
-            return dp.get(index+" "+buying);
-        int cooldown = buySell(index+1, buying);
-        if(buying){
-            int buy = buySell(index+1, !buying) - price[index];
-            dp.put(index+" "+buying,(Math.max(buy,cooldown)));
-        }
-        else {
-            int sell = buySell(index+2, !buying) + price[index];
-            dp.put(index+" "+buying,(Math.max(sell,cooldown)));
-        }
-        return dp.get(index+" "+buying);
-    }
     public int maxProfit(int[] prices) {
-        price=prices;
-        return buySell(0,true);
+        if (prices == null || prices.length <2 ) {
+            return 0;
+        }
+        if (prices.length == 2) {
+            return Math.max(prices[1] - prices[0], 0);
+        }
+        int profit = 0;
+        int[][] dp = new int[prices.length][2];
+        //dp[i][0]: no share at hand at the end of day i
+        //dp[i][1]: has share at the end of day i
+        dp[0][1]  = -prices[0];
+        dp[1][0] = Math.max(0, prices[1] - prices[0]);
+        dp[1][1] = Math.max(-prices[0], -prices[1]);
+        
+        for (int i=2; i<= prices.length-1; i++) { //len >= 3
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-2][0]-prices[i]);//3 days to complete a transaction
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            profit = Math.max(profit, Math.max(dp[i][0], dp[i][1]));
+        }
+        return profit;
     }
 }
-
-
