354cdcfdfff497e045ccafc07d308629ea35a07c
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-08-24 16:10:31 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (70.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,39 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l=0, h=nums.length-1;
+        int l=0, h = nums.length-1;
         while(l<=h){
-            int m = l+(h-l)/2;
-            if(target==nums[m])
-                return m;
-            if(nums[l]<=nums[m]){
-                if(nums[l]<=target && target<nums[m]){
-                    h=m-1;
+            int mid = l +(h-l)/2;
+            //System.out.println(l+" "+mid+" "+h);
+            if(nums[mid]==target)
+                return mid;
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target && target<=nums[mid]){
+                    h=mid-1;
                 }
-                else {
-                    l=m+1;
-                }
+                else
+                    l = mid+1;
             }
             else {
-                if(nums[m]<target && target<=nums[h]){
-                    l=m+1;
+                if(nums[mid]<=target && target<=nums[h]){
+                    l = mid+1;
                 }
-                else {
-                    h=m-1;
-                }
+                else
+                    h=mid-1;
             }
         }
         return -1;
     }
 }
 
-/*
-[3,1]
-1
- l=0 h=1
- m=0
- 
- 
- 
-
-
-*/
