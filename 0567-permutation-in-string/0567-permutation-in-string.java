c4af698394c9d6604ee4d124b393095e86191733
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-08-23 19:05:26 -0700

    Time: 28 ms (36.09%), Space: 43.8 MB (30.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,37 +2,31 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s2.length()<s1.length())
             return false;
-        Map<Character,Integer> mp = new HashMap<>();
-        int k=s1.length(),co=0;
+        Map<Character,Integer> count = new HashMap<>();
         for(int i=0;i<s1.length();i++){
-            mp.put(s1.charAt(i),mp.getOrDefault(s1.charAt(i),0)+1);
+            char ci = s1.charAt(i);
+            char cj = s2.charAt(i);
+            count.put(ci,count.getOrDefault(ci,0)+1);
+            count.put(cj,count.getOrDefault(cj,0)-1);
         }
-        int l=0;
-        for(int i=0;i<k;i++){
-            char c = s2.charAt(i);
-            if(mp.get(c)!=null){    
-                mp.put(c,mp.get(c)-1);
-                if(mp.get(c)==0)
-                    co++;
-            }
-        }
-        if(co==mp.size())
+        if(checkZeros(count))
             return true;
-        for(int i=k;i<s2.length();i++){
-            char c = s2.charAt(i);
-            if(mp.get(s2.charAt(i-k))!=null){
-                if(mp.get(s2.charAt(i-k))==0)
-                    co--;
-                mp.put(s2.charAt(i-k),mp.get(s2.charAt(i-k))+1);
-            }
-            if(mp.get(c)!=null){ 
-                mp.put(c,mp.get(c)-1);
-                if(mp.get(c)==0)
-                    co++;
-            }
-            if(co==mp.size())
+        for(int i=s1.length();i<s2.length();i++){
+            char ci = s2.charAt(i-s1.length());
+            char cj = s2.charAt(i);
+            count.put(cj,count.getOrDefault(cj,0)-1);
+            count.put(ci,count.get(ci)+1);
+            if(checkZeros(count))
                 return true;
         }
+        
         return false;
+    }
+    public boolean checkZeros(Map<Character,Integer> count){
+        for(Map.Entry<Character, Integer> entry : count.entrySet()){
+            if(entry.getValue()!=0)
+                return false;
+        }
+        return true;
     }
 }
