aad98de1b437c3c3b94d1bea105580180882a844
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-09-02 14:41:58 -0700

    Time: 47 ms (86.57%), Space: 54.7 MB (92.51%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -10,8 +10,6 @@ class Solution {
         for(int i=0;i<position.length;i++){
            time[i] = (double)(target-position[i])/(double)(mp.get(position[i]));
         }
-        System.out.println(Arrays.toString(position));
-        System.out.println(Arrays.toString(time));
         double curTime = time[position.length-1];
         for(int i=position.length-2;i>=0;i--){
             if(curTime<time[i]){
