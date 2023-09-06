bb7f5532e472b216b9497e87bc194a8f1c3b8496
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-09-06 16:57:19 -0700

    Time: 416 ms (5.02%), Space: 45.2 MB (15.32%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,54 +1,63 @@
 class Solution {
-    int m,n;
-    boolean atlantic, pacific;
-    public void dfs(int i, int j, int[][] heights, Map<String, Boolean> visited){
-        if(i<0 || i>=m || j<0 || j>=n || (atlantic & pacific))
-            return;
-        if(i-1<0 || j-1<0)
-            pacific =true;
-        if(j+1>=n || i+1>=m)
+    int [][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
+    int n=0,m=0;
+    public boolean bfs(int[][] heights, int i, int j){
+        LinkedList<int[]> q = new LinkedList<>();
+        int [][] visited = new int[n][m];
+        int[] src = new int[2];
+        src[0]=i;
+        src[1]=j;
+        q.add(src);
+        visited[i][j]=1;
+        boolean pacific =false;
+        boolean atlantic = false;
+        if(i==0||j==0)
+            pacific = true;
+        if(i==n-1 || j==m-1)
             atlantic = true;
-        visited.put(i+" "+j,true);
-        //System.out.println("i:"+i +" j:"+j);
-        if((j-1)>=0){
-            if(heights[i][j-1]<=heights[i][j] & visited.get(i+" "+(j-1))==null){
-                dfs(i, j-1, heights,visited);
+        if(atlantic && pacific)
+            return true;
+        while(q.size()>0){
+            int[] pos = q.poll();
+            for(int k=0;k<4;k++){
+                int x = pos[0]+dir[k][0];
+                int y = pos[1]+dir[k][1];
+                int[] v = new int[2];
+                v[0]=x;
+                v[1]=y;
+                if(!(x<0||y<0||x>=n||y>=m)){
+                    if(visited[x][y]==0 && heights[x][y]<=heights[pos[0]][pos[1]]){
+                        if(x==0||y==0)
+                            pacific = true;
+                        if(x==n-1 || y==m-1)
+                            atlantic = true;
+                        if(atlantic && pacific)
+                            return true;
+                        visited[x][y]=1;
+                        q.add(v);
+                    }
+                }
+                
             }
         }
-        if((i+1)<m){
-            if(heights[i+1][j]<=heights[i][j] & visited.get((i+1)+" "+j)==null){
-                dfs(i+1, j, heights,visited);
-            }
-        }
-        if((j+1)<n){ 
-            if(heights[i][j+1]<=heights[i][j] & visited.get(i+" "+(j+1))==null){
-                dfs(i, j+1, heights,visited);
-            }
-        }
-        if((i-1)>=0){
-            if(heights[i-1][j]<=heights[i][j] & visited.get((i-1)+" "+j)==null){
-                dfs(i-1, j, heights,visited);
-            }
-        }
+        return false;
+        
     }
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        m = heights.length;
-        n = heights[0].length;
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0;i<m;i++){
-            for(int j=n-1;j>=0;j--){
-                Map<String, Boolean> visited = new HashMap<>();
-                atlantic = false;
-                pacific = false;
-                dfs(i,j,heights, visited);
-                if(atlantic & pacific){  
-                    List<Integer> coord = new ArrayList<>();
-                    coord.add(i);
-                    coord.add(j);
-                    ans.add(coord);
-                }   
+        n = heights.length;
+        m = heights[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(bfs(heights,i,j)){
+                    List<Integer> pos = new ArrayList<>();
+                    pos.add(i);
+                    pos.add(j);
+                    ans.add(pos);
+                }
             }
         }
         return ans;
+        
     }
 }
