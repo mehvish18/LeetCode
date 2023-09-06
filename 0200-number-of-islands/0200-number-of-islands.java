54d183690cba8a012f802e48a72886ed4cdb28ea
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-09-06 15:55:23 -0700

    Time: 3 ms (79.93%), Space: 47.6 MB (50.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,31 @@
 class Solution {
-    int m,n;
-    int[][] visited;
-    int [][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
-    public void bfs(char[][] grid, int i, int j){
-        visited[i][j]=1;
-        Queue<int[]> q = new LinkedList<>();
-        int [] cord = {i,j};
-        q.add(cord);
-        while(q.size()>0){
-            int [] pos = q.poll();
-            for(int[] d:dir){
-                int x = pos[0]+d[0];
-                int y = pos[1]+d[1];
-                if(x>=0 && x<m && y>=0 && y<n && visited[x][y]==0 && grid[x][y]=='1'){
-                        int[] cords={x,y};
-                        q.add(cords);
-                        visited[x][y]=1;
-                }
-            }
+    int [][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    int n=0,m=0;
+    public void dfs(char[][] grid ,int i, int j){
+        //System.out.println(i+" "+j);
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0')
+            return;
+        grid[i][j] = '0';
+        for(int k=0;k<4;k++){
+           int x = i + dir[k][0];
+           int y = j + dir[k][1];
+           dfs(grid,x,y) ;
         }
-    }
-    public int numIslands(char[][] grid) {
-        int c=0;
-        m = grid.length;
-        n = grid[0].length;
-        visited = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(visited[i][j]==0 && grid[i][j]=='1'){ 
-                    bfs(grid,i,j);
-                    c++;  
-                }
-            }
         
+    }
+    
+    public int numIslands(char[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        int c = 0 ;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    c++;
+                    dfs(grid,i,j);
+                }
+            }
         }
-
         return c;
     }
 }
