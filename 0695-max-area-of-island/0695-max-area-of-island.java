7cd3e067584e843d20b89fd531b1e3c878d6f458
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-09-06 16:04:33 -0700

    Time: 2 ms (80.41%), Space: 43.6 MB (32.09%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,24 +1,33 @@
 class Solution {
-    public int dfs(int[][] grid, int row, int col){
-        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0)
-            return 0;
-        grid[row][col] = 0; // mark as visited
-        
-        // there is no need to give 1 back to grid like gird[row][col] = 1
-		// dont forget to add 1 before return
-        return 1 + dfs(grid, row, col-1) + dfs(grid, row+1, col) + dfs(grid, row, col+1) + dfs(grid, row-1, col);
-    }
-    
-    public int maxAreaOfIsland(int[][] grid) {
-        
-        int max = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1)
-                    max = Math.max(max, dfs(grid, i, j));
-            }
+    int [][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    int n=0,m=0;
+    int c=0;
+    public void dfs(int[][] grid ,int i, int j){
+        //System.out.println(i+" "+j);
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0)
+            return;
+        c++;
+        grid[i][j] = 0;
+        for(int k=0;k<4;k++){
+           int x = i + dir[k][0];
+           int y = j + dir[k][1];
+           dfs(grid,x,y) ;
         }
         
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        int max=0;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                c=0;
+                if(grid[i][j]==1){
+                    dfs(grid,i,j);
+                }
+                max = Math.max(max,c);
+            }
+        }
         return max;
     }
 }
