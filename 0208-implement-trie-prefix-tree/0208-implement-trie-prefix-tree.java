e4c36e377d4626d5279a7dcf2cab2afa358f9df6
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-09-07 15:34:40 -0700

    Time: 71 ms (6.29%), Space: 55.7 MB (7.42%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,13 +1,15 @@
 class Trie {
     public class TreeNode {
         public char value;
-        public List<TreeNode> neighbors;
+        public HashMap<Character,TreeNode> neighbors;
         public TreeNode(){
-            neighbors  = new ArrayList<>();
+            neighbors  = new HashMap<>();
         }
     }
+    
     TreeNode root;
     int depth;
+    
     public Trie() {
         root = new TreeNode();
         depth=0;
@@ -15,48 +17,40 @@ class Trie {
     
     public void insert(String word) {
         depth = Math.max(depth,word.length());
+        
         if(root.neighbors.size()==0){
             List<TreeNode> treeList = new ArrayList<>();
             TreeNode node = new TreeNode();
             char ch = word.charAt(0);
             node.value = ch;
             treeList.add(node);
-            root.neighbors.add(node);
+            root.neighbors.put(ch,node);
             for(int i=1;i<word.length();i++){
                 TreeNode node1 = new TreeNode();
                 ch = word.charAt(i);
                 node1.value = ch;
                 treeList.add(node1);
-                treeList.get(i-1).neighbors.add(node1);      
+                treeList.get(i-1).neighbors.put(ch,node1);      
             }  
-            treeList.get(word.length()-1).neighbors.add(null);
+            treeList.get(word.length()-1).neighbors.put('-',new TreeNode());
         }
         else {
             TreeNode parent = root;
             for(int j=0;j<word.length();j++){
                 boolean letterNotFound = true;
                 char ch = word.charAt(j);
-                for(int i=0;i<parent.neighbors.size();i++){
-                    TreeNode node = parent.neighbors.get(i);
-                    if(node!=null && node.value==ch){
-                        parent = node;
-                        letterNotFound = false;
-                        break;
-                    }
-                }
-                if(letterNotFound){
-                    TreeNode node = new TreeNode();
+                TreeNode node = parent.neighbors.get(ch);
+                if(node==null){
+                    node = new TreeNode();
                     node.value = ch;
-                    parent.neighbors.add(node);
-                    parent=node;
+                    parent.neighbors.put(ch,node);  
                 }
+                parent=node;
             }
             //System.out.println(parent.value+" "+parent.neighbors.size());
-            for(int i=0;i<parent.neighbors.size();i++){
-                if(parent.neighbors.get(i)==null)
+            if(parent.neighbors.get('-')!=null)
                     return;
-            }
-            parent.neighbors.add(null);
+            parent.neighbors.put('-', new TreeNode());
             System.out.println(parent.neighbors.size());
         }
     }
@@ -68,23 +62,15 @@ class Trie {
             for(int j=0;j<word.length();j++){
                 boolean letterNotFound = true;
                 char ch = word.charAt(j);
-                for(int i=0;i<parent.neighbors.size();i++){
-                    TreeNode node = parent.neighbors.get(i);
-                    if(node!=null && node.value==ch){
-                        parent = node;
-                        letterNotFound = false;
-                        break;
-                    }
-                }
-                if(letterNotFound){
+                parent = parent.neighbors.get(ch);
+                if(parent==null){
                     return false;
                 }
+                
             }
             //System.out.println(parent.value+" "+parent.neighbors.size());
-            for(int i=0;i<parent.neighbors.size();i++){
-                if(parent.neighbors.get(i)==null)
+            if(parent.neighbors.get('-')!=null)
                     return true;
-            }
             return false;
     }
     
@@ -93,15 +79,8 @@ class Trie {
             for(int j=0;j<prefix.length();j++){
                 boolean letterNotFound = true;
                 char ch = prefix.charAt(j);
-                for(int i=0;i<parent.neighbors.size();i++){
-                    TreeNode node = parent.neighbors.get(i);
-                    if(node!=null && node.value==ch){
-                        parent = node;
-                        letterNotFound = false;
-                        break;
-                    }
-                }
-                if(letterNotFound){
+                parent = parent.neighbors.get(ch);
+                if(parent==null){
                     return false;
                 }
             }
