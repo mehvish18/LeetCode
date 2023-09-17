cd29a58b8f9be89efa28578cb94ad2469ba86e79
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-09-17 13:16:06 -0700

    Time: 7 ms (42.60%), Space: 44.9 MB (35.75%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,38 +1,40 @@
 class Solution {
-    
-    public int[] findOrder(int n, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-        int[] toplogical = new int[n];
-        int k=0;
-        Queue<Integer> q = new LinkedList<Integer>();
-        int [] deg = new int[n];
-        for(int i=0;i<n;i++){
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
+        for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
         for(int i=0;i<prerequisites.length;i++){
-            deg[prerequisites[i][0]]++;
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
-        for(int i=0;i<n;i++){
-            if(deg[i]==0) {
-                toplogical[k]=i;
-                k++;
+        LinkedList<Integer> q = new LinkedList<>();
+        int [] inDegree = new int [numCourses];
+        int [] topSort = new int[numCourses];
+        
+        for(int i=0;i<numCourses;i++){
+            for(int j=0;j<adj.get(i).size();j++){
+                inDegree[adj.get(i).get(j)]++;
+            }
+        }
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0){
                 q.add(i);
             }
         }
         
-        while(!q.isEmpty()){
-            int node = q.poll();
-            for(int i=0;i<adj.get(node).size();i++){
-                if(--deg[adj.get(node).get(i)]==0){
-                    q.add(adj.get(node).get(i));
-                    toplogical[k]=adj.get(node).get(i);
-                    k++;
+        int cnt =0;
+        while(q.size()>0){
+            int u = q.poll();
+            topSort[cnt]=u;
+            cnt++;
+            for(int v:adj.get(u)){
+                if(--inDegree[v]==0){
+                    q.add(v);
                 }
             }
         }
-        if(k<n)
-            return new int[] {};
-        return toplogical;
+        if(cnt==numCourses)
+            return topSort;
+        return new int[0];
     }
 }
