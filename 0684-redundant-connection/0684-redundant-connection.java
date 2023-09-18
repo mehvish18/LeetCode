47e4885788d3399e5c0d6d2c8032e039d10a8cb2
mehvish18 <49357254+mehvish18@users.noreply.github.com>
2023-09-17 17:36:17 -0700

    Time: 15 ms (5.22%), Space: 44.1 MB (18.04%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,26 +1,91 @@
 class Solution {
-    int find(int [] parent,int i){
-        if(parent[i]==i)
-            return i;
-        return find(parent,parent[i]);
-    }
-    void union(int [] parent, int x,int y){
-        parent[x]=y;
-    }
-    
     public int[] findRedundantConnection(int[][] edges) {
-        int parent[] = new int[edges.length];
-        for(int i=0; i<edges.length;i++){
-            parent[i] =i;
+        Map<String, Integer> mp = new HashMap<>();
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        int n = edges.length-1;
+        int root =edges[0][0];
+        for(int i=0;i<edges.length;i++){
+            mp.put(edges[i][0]+" "+edges[i][1], i);
+            mp.put(edges[i][1]+" "+edges[i][0], i);
+            List<Integer> lst = adj.getOrDefault(edges[i][0], new ArrayList<>());
+            lst.add(edges[i][1]);
+            adj.put(edges[i][0], lst);
+            lst = adj.getOrDefault(edges[i][1], new ArrayList<>());
+            lst.add(edges[i][0]);
+            adj.put(edges[i][1],lst);
         }
-        for (int i = 0; i<edges.length; i++) {
-            int x = find(parent,edges[i][0]-1);
-            int y = find(parent,edges[i][1]-1);
-            if (x == y)
-                return edges[i];
- 
-            union(parent, x, y);
+        int [] parent = new int[adj.size()+1];
+        boolean [] visited = new boolean[adj.size()+1];
+        LinkedList<Integer> q = new LinkedList<>();
+        q.add(1);
+        visited[1]=true;
+        parent[1]=1;
+        int u=0,v=0;
+        while(q.size()>0){
+            u = q.poll();
+            for(int i=0;i<adj.get(u).size();i++){
+                v=adj.get(u).get(i);
+                if(!visited[v]){
+                    visited[v]=true;
+                    parent[v]=u;
+                    q.add(v);
+                }
+                else if(parent[u]!=v){
+                    q = new LinkedList<>();
+                    break;
+                }
+            }
         }
-        return new int[]{};
+        if((mp.get(u+" "+v)!=null && mp.get(u+" "+v)==n)||(mp.get(v+" "+u)!=null && mp.get(v+" "+u)==n))
+            return edges[n];
+        int max=mp.get(u+" "+v);
+        //System.out.println("ans "+u+" "+v);
+        //System.out.println(Arrays.toString(parent));
+        HashSet<Integer> us = new HashSet<>();
+        us.add(u);
+        HashSet<Integer> vs = new HashSet<>();
+        vs.add(v);
+        int node=0;
+        int u1=u,v1=v;
+        while(true){
+            //System.out.println(u+" "+v);
+            if(us.contains(v)) {
+               node = v;
+               break;
+            }
+            if(vs.contains(u)) {
+               node=u;
+               break;
+            }
+            v = parent[v];
+            u = parent[u];
+            us.add(u);
+            vs.add(v);
+        }
+        v=v1;
+        u=u1;
+        //System.out.println(u+" "+v+" "+ node);
+        while(u!=node && v!=node){
+            if(v!=node){
+                if(mp.get(parent[v]+" "+v)!=null && mp.get(parent[v]+" "+v)>max)
+                    max=mp.get(parent[v]+" "+v);
+                v = parent[v];
+            }
+            if(u!=node) {
+                if(mp.get(parent[u]+" "+u)!=null && mp.get(parent[u]+" "+u)>max)
+                    max=mp.get(parent[u]+" "+u);
+                u = parent[u];
+            }
+            //System.out.println(u+" "+v);
+        }
+        return edges[max];
     }
 }
+
+/*
+
+1 2 3 4 5
+1 1   1
+
+
+*/
